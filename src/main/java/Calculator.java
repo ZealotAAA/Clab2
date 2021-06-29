947c2134949f230d68d2a4e546c20002ce984f3d
Xu <xyp4913@gmail.com>
2021-06-29 02:49:30 +0200

    fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,6 @@ import java.io.IOException;
  */
 public class Calculator {
     public static void main(String[] args) throws IOException {
-            CalculatorConsole.run();
+           main.java.CalculatorConsole.run();
     }
 }
